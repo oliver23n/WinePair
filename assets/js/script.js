082fb54df27321a7e402692727f6d4301379222c
oliver23n <oliver.nasevski@gmail.com>
2023-05-09 19:39:39 -0400

    apis connected

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-//pair the wine
+//pair the wine api (Based on the type of wine generate food items)
 const settings = {
     async: true,
     crossDomain: true,
@@ -11,5 +11,21 @@ const settings = {
 };
 
 $.ajax(settings).done(function (response) {
+    console.log(response);
+});
+
+//generate recipes api 
+const settings1 = {
+    async: true,
+    crossDomain: true,
+    url: 'https://edamam-recipe-search.p.rapidapi.com/search?q=chicken',
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '5932463005msha3ed8e2f210665ep1e2f5cjsne6bc7cc14e2c',
+        'X-RapidAPI-Host': 'edamam-recipe-search.p.rapidapi.com'
+    }
+};
+
+$.ajax(settings1).done(function (response) {
     console.log(response);
 });
