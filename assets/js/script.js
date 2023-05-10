c3a523622265b4ef7c67204fa9da01d0dad2ac05
oliver23n <oliver.nasevski@gmail.com>
2023-05-09 21:02:29 -0400

    getting data from the type wine api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,54 @@
-//pair the wine api (Based on the type of wine generate food items)
+
 let wineType;
 let fooditem;
-const settings = {
-    async: true,
-    crossDomain: true,
-    url: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/wine/dishes?wine='+wineType,
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': '5932463005msha3ed8e2f210665ep1e2f5cjsne6bc7cc14e2c',
-        'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
-    }
-};
 
-$.ajax(settings).done(function (response) {
-    console.log(response);
-});
 
-//generate recipes api 
-const settings1 = {
-    async: true,
-    crossDomain: true,
-    url: 'https://edamam-recipe-search.p.rapidapi.com/search?q='+fooditem,
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': '5932463005msha3ed8e2f210665ep1e2f5cjsne6bc7cc14e2c',
-        'X-RapidAPI-Host': 'edamam-recipe-search.p.rapidapi.com'
-    }
-};
-
-$.ajax(settings1).done(function (response) {
-    console.log(response);
-});
 function getWine(){
     wineType = $('#inputWine').val();
-    console.log(wineType)
+    passWine(wineType);
 }
-$('#submitB').on('click',getWine);
+function passWine(type){
+    //pair the wine api (Based on the type of wine generate food items)
+    const settings = {
+        async: true,
+        crossDomain: true,
+        url: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/wine/dishes?wine=' + type,
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '5932463005msha3ed8e2f210665ep1e2f5cjsne6bc7cc14e2c',
+            'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+        }
+    };
 
+    $.ajax(settings).done(function (response) {
+
+
+        console.log(response);
+
+        //generate recipes api 
+        const settings1 = {
+            async: true,
+            crossDomain: true,
+            url: 'https://edamam-recipe-search.p.rapidapi.com/search?q=' + fooditem,
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': '5932463005msha3ed8e2f210665ep1e2f5cjsne6bc7cc14e2c',
+                'X-RapidAPI-Host': 'edamam-recipe-search.p.rapidapi.com'
+            }
+        };
+
+        $.ajax(settings1).done(function (response1) {
+            console.log(response1);
+        });
+    });
+}
+function init(){
+        $('#submitB').on('click',getWine);
+      
+
+    
+}
+init();
 
 // Get the input for the wine 
 // generate the food items
